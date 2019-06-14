1a5f2b19dbfdb6820561f4d3174ae2ab85ec30a0
timonvandieren <51820003+timonvandieren@users.noreply.github.com>
2019-06-14 12:03:01 +0200

    Fix DeprecationWarning time.clock python 3.8

--- nptdms/utils.py
+++ nptdms/utils.py
@@ -7,9 +7,9 @@ class Timer(object):
         self._description = description
 
     def __enter__(self):
-        self._start_time = time.clock()
+        self._start_time = time.perf_counter()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        elapsed_time = (time.clock() - self._start_time) * 1.0e3
+        elapsed_time = (time.perf_counter() - self._start_time) * 1.0e3
         self._log.info("{0}: Took {1} ms".format(
             self._description, elapsed_time))
